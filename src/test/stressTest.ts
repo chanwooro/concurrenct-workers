4298bef1f77095ebcbab2e4d467e24ce41268e93
Chan Ro <chan.ro@Chans-MacBook-Pro.local>
2023-10-01 17:49:25 +1300

    test cases update

--- src/test/stressTest.ts
+++ src/test/stressTest.ts
@@ -1,371 +1,368 @@
 import { Runner } from "../Runner";
 
 const process = [
-  () => new Promise((resolve) => setTimeout(() => resolve(
-      "aaa"
-  ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "aaa"
   ), 30000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "bbb"
   ), 40000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ccc"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "ddd"
   ), 10000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "eee"
   ), 2000)),
-  () => new Promise((resolve) => setTimeout(() => resolve(
+  new Promise((resolve) => setTimeout(() => resolve(
       "fff"
   ), 5000))
   
 ]
 
 const run = async () => {
-  const testC = new Runner(process, 10, 60000);
+  const testC = new Runner(process, 10, 10000);
   const result = await testC.execute();
   console.log("resi;ts", result)
 }
